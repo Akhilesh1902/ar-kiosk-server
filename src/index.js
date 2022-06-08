1f7a6c5236345df075b6d1d3cb07e97621be1fe9
Akhilesh1902 <akhil1922002@gmail.com>
2022-06-08 20:27:24 +0530

    implemented cors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 console.log('hello there');
 import Express from 'express';
 import http from 'http';
+import cors from 'cors';
 
 const app = Express();
-
+app.use(cors());
 // adding /static as the prefix for the image url
 // path to user : http://localhost:2020/static/images/darshan.jpeg
 app.use('/static', Express.static('public'));
 
 const PORT = 2020;
 
-const server = http.createServer(app);
+const server = http.createServer(app, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET'],
+  },
+});
 
 app.get('/', (req, res) => {
   res.send('hello three');
